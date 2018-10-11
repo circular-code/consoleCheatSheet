2961879f799902ad3448b8b8f63b48366b617c41
circular-code <stephanos@malikotsinas.de>
2018-10-11 10:11:41 +0200

    changed order, added seperation

--- console.js
+++ console.js
@@ -4,8 +4,8 @@ console.log("%cBasic Output", "color: white; font-size: 15px; background-color: 
 console.log("You may use string substitution with these methods. See chapter 'String Substitution' below.");
 
 console.group("log");
-console.log("%clog", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c(obj1 ... objN:object || (msg:string, subst1...substN:object))", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%clog", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c(obj1 ... objN:object || (msg:string, subst1...substN:object))", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 var car = {name: "firebird", brand: "pontiac", color: "red"};
@@ -21,8 +21,8 @@ console.log("");
 console.groupEnd("log");
 console.group("info");
 
-console.log("%cinfo", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c(obj1 ... objN:object || (msg:string, subst1...substN:object))", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%cinfo", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c(obj1 ... objN:object || (msg:string, subst1...substN:object))", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 console.info("You should know about this, so here have some information.");
@@ -35,8 +35,8 @@ console.log("");
 console.groupEnd("info");
 console.group("warn");
 
-console.log("%cwarn", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c(obj1 ... objN:object || (msg:string, subst1...substN:object))", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%cwarn", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c(obj1 ... objN:object || (msg:string, subst1...substN:object))", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 console.warn("There might be a risky thing here. Please check it out.");
@@ -49,8 +49,8 @@ console.log("");
 console.groupEnd("warn");
 console.group("error, exception (deprecated)");
 
-console.log("%cerror, exception (deprecated)", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px;");
-console.log("%c(obj1 ... objN:object || (msg:string, subst1...substN:object))", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%cerror, exception (deprecated)", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px;");
+console.log("%c(obj1 ... objN:object || (msg:string, subst1...substN:object))", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 console.error("Something went horribly wrong! :(");
@@ -63,12 +63,23 @@ console.log("");
 
 console.groupEnd("error, exception (deprecated)");
 
+console.log("%cString Substitution", "color: white; font-size: 15px; background-color: DarkOrange;padding: 5px;margin-top: 40px;");
+
+console.group("style");
+
+console.log("%cstyle", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("");
+
+console.log('%cHello world', 'font-size:40px;color:#fff;text-shadow:0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);');
+
+console.groupEnd("style");
+
 console.log("%cLogging Objects", "color: white; font-size: 15px; background-color: DarkOrange;padding: 5px;margin-top: 40px;");
 
 console.group("dir");
 
-console.log("%cdir", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c(object:object)", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%cdir", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c(object:object)", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 console.dir(car);
@@ -79,8 +90,8 @@ console.log("");
 console.groupEnd("dir");
 console.group("dirxml");
 
-console.log("%cdirxml", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c(object:object)", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%cdirxml", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c(object:object)", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 console.dirxml(car);
@@ -91,7 +102,7 @@ console.log("");
 console.groupEnd("dirxml");
 console.group("table");
 
-console.log("%ctable", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
+console.log("%ctable", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
 console.log("");
 
 console.log("an array of arrays");
@@ -135,10 +146,46 @@ console.groupEnd("table");
 
 console.log("%cUtility", "color: white; font-size: 15px; background-color: DarkOrange;padding: 5px;margin-top: 40px;");
 
+
+console.group("group");
+
+console.log("%cgroup, groupCollapsed, groupEnd", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c(label(optional):string)", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
+console.log("");
+
+console.group("outer level");
+console.groupCollapsed("inner level (collapsed)");
+console.warn("warning inside inner level");
+console.groupEnd("inner level (collapsed)");
+console.groupEnd("outer level");
+
+console.log("");
+console.log("");
+
+console.groupEnd("group");
+console.group("trace");
+
+console.log("%ctrace", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("");
+
+function foo() {
+  function bar() {
+    console.trace();
+  }
+  bar();
+}
+
+foo();
+
+console.log("");
+console.log("");
+
+console.groupEnd("trace");
+
 console.group("assert");
 
-console.log("%cassert", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c(assertion:bool, obj1 ... objN:object || (msg:string, subst1...substN:object))", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%cassert", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c(assertion:bool, obj1 ... objN:object || (msg:string, subst1...substN:object))", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 var errorMsg = 'the # is not even';
@@ -154,8 +201,8 @@ console.log("");
 console.groupEnd("assert");
 console.group("clear");
 
-console.log("%cclear", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c()", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%cclear", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c()", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 console.log("clears the console");
@@ -165,10 +212,13 @@ console.log("");
 console.log("");
 
 console.groupEnd("clear");
+
+console.log("%cPerformance", "color: white; font-size: 15px; background-color: DarkOrange;padding: 5px;margin-top: 40px;");
+
 console.group("count");
 
-console.log("%ccount, countReset", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c(label(optional):string)", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%ccount, countReset", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c(label(optional):string)", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 (function() {
@@ -221,52 +271,10 @@ console.log("");
 console.log("");
 
 console.groupEnd("count");
-console.group("group");
-
-console.log("%cgroup, groupCollapsed, groupEnd", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c(label(optional):string)", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
-console.log("");
-
-console.group("outer level");
-console.groupCollapsed("inner level (collapsed)");
-console.warn("warning inside inner level");
-console.groupEnd("inner level (collapsed)");
-console.groupEnd("outer level");
-
-console.log("");
-console.log("");
-
-console.groupEnd("group");
-console.group("trace");
-
-console.log("%ctrace", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("");
-
-function foo() {
-  function bar() {
-    console.trace();
-  }
-  bar();
-}
-
-foo();
-
-console.log("");
-console.log("");
-
-console.groupEnd("trace");
-console.group("style");
-
-console.log("%cstyle", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("");
-
-console.log('%cHello world', 'font-size:40px;color:#fff;text-shadow:0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);');
-
-console.groupEnd("style");
 console.group("time");
 
-console.log("%ctime, timeLog, timeEnd", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c(label(optional):string)", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%ctime, timeLog, timeEnd", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c(label(optional):string)", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 console.time();
@@ -280,9 +288,9 @@ console.log("");
 console.groupEnd("time");
 console.group("profile, profileEnd");
 
-console.log("%cNot standardized", "color: white; font-size: 15px; background-color: crimson;padding: 5px");
-console.log("%cprofile, profileEnd", "color: white; font-size: 15px; background-color: cornflowerblue;padding: 5px");
-console.log("%c(label(optional):string)", "color: white; font-size: 15px; background-color: LightSeaGreen;padding: 5px");
+console.log("%cNot standardized", "color: white; font-size: 12px; background-color: crimson;padding: 5px");
+console.log("%cprofile, profileEnd", "color: white; font-size: 12px; background-color: cornflowerblue;padding: 5px");
+console.log("%c(label(optional):string)", "color: white; font-size: 12px; background-color: LightSeaGreen;padding: 5px");
 console.log("");
 
 console.profile();
